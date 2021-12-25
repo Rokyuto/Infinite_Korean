1dbf86c697661c7e5cef3343500ae00f20464908
Rokyuto <viktorvalerievasenov6@gmail.com>
2021-12-25 20:27:58 +0200

    Initial Commit

--- Infinite_Korean/MainPage.xaml.cs
+++ Infinite_Korean/MainPage.xaml.cs
@@ -4,15 +4,18 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Infinite_Korean
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
         }
+
     }
 }
